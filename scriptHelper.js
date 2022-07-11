d6f67e18e1e31c33521ca9e66d8eb96b66729ae1
S W <sw@Ss-MacBook-Air.local>
2022-07-11 17:36:28 -0500

    Cap error

--- scriptHelper.js
+++ scriptHelper.js
@@ -93,7 +93,7 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-    let planet = planets[Math.floor(math.random()*planets.length)];
+    let planet = planets[Math.floor(Math.random()*planets.length)];
     return planet
 }
 
